6f428fe8992df1e36f00c830d5d2a05e8fb540b6
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-01-23 10:35:39 +0100

    Dockerize all the things

--- marske/run_steps.sh
+++ marske/run_steps.sh
@@ -2,9 +2,16 @@
 
 # Function to delete a file if it exists
 delete_if_exists() {
-  if [ -e "$1" ]; then
-    rm "$1"
-  fi
+	if [ -e "$1" ]; then
+		rm "$1"
+	fi
+}
+
+# Function to install TypeScript and npm dependencies
+install_dependencies() {
+	echo "Installing TypeScript and npm dependencies..."
+	npm install -g typescript
+	npm install
 }
 
 # Delete existing files because we're going to recreate them
@@ -14,34 +21,37 @@ delete_if_exists "typescript_quine.ts"
 delete_if_exists "python_quine.py"
 delete_if_exists "quine.js"
 
+# Install dependencies
+install_dependencies
+
 # Loop through the steps
 for step in {1..5}; do
-  case $step in
-  1)
-    # Step 1: Compile TypeScript file
-    delete_if_exists "quine.js"
-    tsc quine.ts
-    ;;
+	case $step in
+	1)
+		# Step 1: Compile TypeScript file
+		delete_if_exists "quine.js"
+		tsc quine.ts
+		;;
 
-  2)
-    # Step 2: Execute TypeScript output
-    node quine.js
-    ;;
+	2)
+		# Step 2: Execute TypeScript output
+		node quine.js
+		;;
 
-  3)
-    # Step 3: Compile Rust file
-    delete_if_exists "rust_quine"
-    rustc rust_quine.rs
-    ;;
+	3)
+		# Step 3: Compile Rust file
+		delete_if_exists "rust_quine"
+		rustc rust_quine.rs
+		;;
 
-  4)
-    # Step 4: Execute Rust output
-    ./rust_quine
-    ;;
+	4)
+		# Step 4: Execute Rust output
+		./rust_quine
+		;;
 
-  5)
-    # Step 5: Execute Python script
-    python python_quine.py
-    ;;
-  esac
+	5)
+		# Step 5: Execute Python script
+		python python_quine.py
+		;;
+	esac
 done
